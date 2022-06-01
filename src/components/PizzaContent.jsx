fde0af75e5f50356b6a8d07b84e2460572a3e618
rustamgadjiev <rustamgadjiev@list.ru>
2022-06-02 00:21:58 +0300

    добавил получения масива пицц с сервера

--- src/components/PizzaContent.jsx
+++ src/components/PizzaContent.jsx
@@ -1,12 +1,26 @@
-import pizzas from '../utils/Pizza-List.json';
-import { Pizza } from './Pizza';
+import { useEffect, useState } from "react";
+import { PIZZA_LIST_URL } from "../utils/constants";
+import { Pizza } from "./Pizza";
 
 export const PizzaContent = () => {
+  const [pizzaList, setPizzaList] = useState([]);
+
+  useEffect(() => {
+    try {
+      fetch(PIZZA_LIST_URL)
+        .then((resp) => resp.json())
+        .then((pizzaList) => setPizzaList(pizzaList))
+        .catch(console.log);
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
+
   return (
     <>
       <h2 className="content__title">Все пиццы</h2>
       <div className="content__items">
-        {pizzas.map((pizza) => {
+        {pizzaList.map((pizza) => {
           return <Pizza key={pizza.id} {...pizza} />;
         })}
       </div>
